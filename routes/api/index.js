d2a9a0366fb547072a5143ddc5932a703bddd6de
Topash15 <alextopash@gmail.com>
2021-08-31 21:38:41 -0400

    posting note will write to db file

--- routes/api/index.js
+++ routes/api/index.js
@@ -1,30 +1,39 @@
-const express = require('express');
+const fs = require("fs");
+const path = require("path");
+const express = require("express");
 const router = express.Router();
-const db = require('../../db/db.json');
+const db = require("../../db/db.json");
 
 //gets notes list
-router.get('/', (req, res) => {
-    res.json(db)
-})
+router.get("/", (req, res) => {
+  res.json(db);
+  return db;
+});
 
 //adds note to notes list
-router.post('/', (req, res) => {
-    //save post to json variable and add json to notes list
-    const newNote = {
-        title : req.body.title,
-        text : req.body.text,
-    }
+router.post("/", (req, res) => {
+  //save post to json variable and add json to notes list
+  const newNote = {
+    title: req.body.title,
+    text: req.body.text,
+  };
 
-    //checks for title and text
-    if (!newNote.title || !newNote.text){
-      return res.status(400).json({ msg: "Please include title and description."})
-    }
+  //checks for title and text
+  //returns error if no input
+  if (!newNote.title || !newNote.text) {
+    return res
+      .status(400)
+      .json({ msg: "Please include title and description." });
+  }
 
-    db.push(newNote);
-    res.send(db);
-})
+  //adds new note to db list and returns list
+  db.push(newNote);
+  const dbLocation = path.join(__dirname, "../../db/db.json");
+  //writes new note into db list
+  fs.writeFileSync(dbLocation, JSON.stringify(db));
+  res.send(db);
+});
 
 //deletes note from notes list
-
 
 module.exports = router;
--- server.js
+++ server.js
@@ -1,29 +1,22 @@
 //Dependencies
-const express = require('express');
-const path = require('path');
+const express = require("express");
+const path = require("path");
 
 const PORT = process.env.PORT || 3001;
 const app = express();
 
+//body parser middleware
 app.use(express.json());
 app.use(express.urlencoded({ extended: false }));
 
-app.use(express.static(path.join(__dirname, 'public')));
+//sets static path
+app.use(express.static(path.join(__dirname, "public")));
 
 // Use api router
-app.use('/api/db', require('./routes/api/index'));
+app.use("/api/db", require("./routes/api/index"));
 
 //use html router
-app.use('/', require('./routes/html/index'));
-
-
-// app.get('/', (req, res) => {
-//   res.sendFile(path.join(__dirname + './index.html'))
-// });
-
-// app.get('/notes', (req, res) => {
-//     res.sendFile(path.join(__dirname + './notes.html'))
-// });
+app.use("/", require("./routes/html/index"));
 
 // Listener
 app.listen(PORT, () => {
